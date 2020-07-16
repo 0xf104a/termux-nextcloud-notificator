98bb27c2bb819b8234eb5317358fd7b6ad2b10f3
Andrew Zaitsew <toddot@live.ru>
2020-07-16 21:41:26 +0300

    Replaced sanitization with IO::popen

--- termux.rb
+++ termux.rb
@@ -3,11 +3,11 @@ def sanitize(s)
 end
 module Termux
    def self.open_url(url)
-       `termux-open-url #{url.dump()}`
+       IO::popen(["termux-open-url",url])
    end
    
    def self.notify(content, group, n_id, title, image)
         print("termux-notification -c #{sanitize(content)} --title #{sanitize(title)} --id #{n_id} --group #{sanitize(group)}\n")
-	IO::popen(["termux-notification", "-c",content, "--title", title, "--id", n_id, "--group", "group"])`
+	IO::popen(["termux-notification", "-c",content, "--title", title, "--id", n_id, "--group", "group"])
    end
 end
