d8997777d2f953e381689d9c02b8d20b79c65221
Andrew Zaitsew <toddot@live.ru>
2020-07-16 19:54:50 +0300

    Fixed termux module

--- termux.rb
+++ termux.rb
@@ -1,10 +1,10 @@
 
 module Termux
-   def open_url(url)
+   def self.open_url(url)
        `termux-open-url #{url.dump()}`
    end
    
-   def notify(content, group, id, title, image)
+   def self.notify(content, group, id, title, image)
 	`termux-notification -c #{content.dump()} --group #{group.dump()} -i #{id} --title #{title.dump()} --image-path #{image.dump()}`
    end
 end
