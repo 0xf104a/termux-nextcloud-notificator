4c6a1cd0f78ed934ae75b916ae67597e39aad7a8
Andrew Zaitsew <toddot@live.ru>
2020-07-22 19:15:42 +0300

    Loglevels added

--- logging.rb
+++ logging.rb
@@ -15,20 +15,45 @@ module Logging
  RED = "\033[31m"
  ENDC = "\033[39m\033[0m"
  BOLD = "\033[1m" 
- @file=nil
+ LEVELS = {"debug"=>4, 
+	   "info" =>3,
+           "warn" =>2,
+           "error"=>1,
+           "none" =>0}
+
  def self.info(message)
+   loglevel=Config::get("logging.level")
+   loglevel=LEVELS[loglevel]
+   if loglevel<3 then
+      return
+   end
    self.write2file("INFO",message)
    print("\r#{BLUE}#{BOLD}INFO  #{ENDC+BOLD}| #{get_time}:#{ENDC}#{message}\n")
  end
  def self.debug(message)
+   loglevel=Config::get("logging.level")
+   loglevel=LEVELS[loglevel]
+   if loglevel<4 then
+      return
+   end   
    self.write2file("DEBUG",message)
    print("\r#{GREEN}#{BOLD}DEBUG #{ENDC+BOLD}| #{get_time}:#{ENDC}#{message}\n")
  end 
  def self.warn(message)
+   loglevel=Config::get("logging.level")
+   loglevel=LEVELS[loglevel]
+   if loglevel<2 then
+      return
+   end
    self.write2file("WARN",message)
    print("\r#{ORANGE}#{BOLD}WARN  #{ENDC+BOLD}| #{get_time}:#{ENDC}#{message}\n")
  end
  def self.error(message)
+   loglevel=Config::get("logging.level")
+   loglevel=LEVELS[loglevel]
+   if loglevel<1 then
+      return
+   end
    self.write2file("ERROR",message)
    print("\r#{RED}#{BOLD}ERROR #{ENDC+BOLD}| #{get_time}:#{ENDC}#{message}\n")
  end
