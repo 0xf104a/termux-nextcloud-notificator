4d1013da6c85badd4126c9d34b6ec2af378b828f
Andrew Zaitsew <toddot@live.ru>
2020-07-23 13:55:31 +0300

    Improved boot sequence

--- run.rb
+++ run.rb
@@ -1,6 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/env ruby
 # Nextcloud notifcation poller for termux
+# Prepare to loading modules
+
+path = File.expand_path File.dirname(__FILE__) 
+Dir.chdir(path)
+
 $LOAD_PATH<<"."
+
+# Load modules
 
 require 'fileutils'
 require 'logging.rb'
@@ -10,14 +17,16 @@ require 'config.rb'
 
 CONFIGFILE="/data/data/com.termux/files/usr/etc/nextcloud-notificator.yml"
 
+# Load config and logging
 Config::load(CONFIGFILE)
-Logging::init()
+Logging::init() # Say logger that config is now available
 
-FileUtils.mkdir_p(Config::get("app.cache"))
+FileUtils.mkdir_p(Config::get("app.cache")) # Create dirs if not exist
 
-appPassword=Config::get("app.password")
+appPassword=Config::get("app.password") # Get place where to store auth data
 
-if not File.file?(appPassword) then
+if not File.file?(appPassword) then 
+   # We have not been authenticated yet
    Logging::warn("It seems user was not authenticated before -- initiating authentication proccess")
    File.open(appPassword,"w+") do |file|
        file.write(Auth::authenticate(Config::get("cloud.endpoint")))
@@ -26,8 +35,9 @@ end
 
 password=File.read(appPassword)
 Logging::info("Succesfully read app password from file")
+
 begin
-  Notifications::start_polling(Config::get("cloud.endpoint"), Config::get("cloud.user"), password, Config::get("polling.interval"))
+  Notifications::start_polling(Config::get("cloud.endpoint"), Config::get("cloud.user"), password, Config::get("polling.interval")) # Start polling
 rescue Interrupt
   Logging::info("User requested stop. Exiting gracefully...")
 end
